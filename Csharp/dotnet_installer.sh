27180ed61307c6355b17c08ca234031b646c9bab
Mike <80423783+MikeTheHash@users.noreply.github.com>
2022-08-11 10:01:27 +0000

    Create dotnet_installer.sh

---
+++ Csharp/dotnet_installer.sh
@@ -0,0 +1,5 @@
+wget https://packages.microsoft.com/config/ubuntu/22.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+sudo dpkg -i packages-microsoft-prod.deb
+rm packages-microsoft-prod.deb
+
+sudo apt-get install -y dotnet-runtime-6.0
