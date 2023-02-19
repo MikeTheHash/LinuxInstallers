1f5d791d7e95484ce9af3d515a8b92e90c889d19
Mike <80423783+MikeTheHash@users.noreply.github.com>
2023-02-19 12:01:19 +0000

    Installer for ProtonVPN

---
+++ Software/ProtonVPN.sh
@@ -0,0 +1,4 @@
+wget https://repo.protonvpn.com/debian/dists/stable/main/binary-all/protonvpn-stable-release_1.0.3_all.deb
+sudo dpkg -i protonvpn-stable-release_1.0.3_all.deb
+sudo apt update
+sudo apt install protonvpn -y
