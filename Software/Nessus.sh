c673743e0b40bc8ac3ffebd58ca5b833f9596f16
Mike <80423783+MikeTheHash@users.noreply.github.com>
2023-02-26 12:09:03 +0000

    Little change
    Added the possibility to start Nessus daemon after the installation

--- Software/Nessus.sh
+++ Software/Nessus.sh
@@ -2,6 +2,17 @@ if [ $USER == "root" ];
 then
   wget https://www.tenable.com/downloads/api/v1/public/pages/nessus/downloads/18394/download?i_agree_to_tenable_license_agreement=true
   dpkg -i Nessus-10.4.2-ubuntu1404_amd64.deb
+  systemctl status nessusd.service
+  read -p "[?] Do you want to active Nessus service? > " usrinput
+  if [ $usrinput == "Y" ];
+  then
+    systemctl start nessusd.service
+  elif [ $usrinput == "y" ];
+  then
+    systemctl start nessusd.service
+  else
+    echo "Okay, happy hacking sir!"
+  fi
 else
   echo "[!] You need to run this as root"
 fi
