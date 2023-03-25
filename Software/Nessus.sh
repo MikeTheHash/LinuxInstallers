e3af895decf92879ff1821b98162c0499482ffb0
Mike <80423783+MikeTheHash@users.noreply.github.com>
2023-03-25 15:27:47 +0000

    Update Nessus.sh

--- Software/Nessus.sh
+++ Software/Nessus.sh
@@ -1,6 +1,6 @@
 if [ $USER == "root" ];
 then
-  wget https://www.tenable.com/downloads/api/v1/public/pages/nessus/downloads/18394/download?i_agree_to_tenable_license_agreement=true
+  wget https://www.tenable.com/downloads/api/v1/public/pages/nessus/downloads/18943/download?i_agree_to_tenable_license_agreement=true
   dpkg -i Nessus-10.4.2-ubuntu1404_amd64.deb
   systemctl status nessusd.service
   read -p "[?] Do you want to active Nessus service? > " usrinput
